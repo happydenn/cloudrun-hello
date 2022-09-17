99c14a7358087ef2d61b29ddc1c4833d16dc53e1
Denny Tsai <dennytsai@google.com>
2022-09-17 02:12:43 +0000

    Add revision id and instance id output

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const { nanoid } = require('nanoid');
 
 const app = express();
 
+const revId = process.env.K_REVISION;
+const instanceId = nanoid();
+
 app.get('/', (req, res) => {
-  res.send('Hello Cloud Run!');
+  res.type('text/plain');
+  res.send(`Hello Cloud Run!\nRevision: ${revId}\nInstance ID: ${instanceId}\n`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-  console.log('Server started.');
+  console.log(`Server started. Revision=${revId} InstanceID=${instanceId}`);
 });
